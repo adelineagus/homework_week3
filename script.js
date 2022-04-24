cbc9ec9a1a01288ce6bb5b47c460dbef8ed1d159
Adeline <adelineaguspranoto@gmail.com>
2022-04-23 18:27:30 -0700

    add criteria validation and re-prompt for length

--- script.js
+++ script.js
@@ -13,21 +13,27 @@ function writePassword() {
 var lowerCase= "abcdefghijklmnopqrstuvwxyz"
 var upperCase= lowerCase.toUpperCase();
 var numbers= "1234567890";
+var specialChar= "!#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
 var randomNum="";
-
 var chars="";
+var validation =false;
 
 function generatePassword() {
   var randomPass="";
   var passwordLength= prompt("choose password length between 8-128 characters");
-  while (passwordLength < 8 || passwordLength > 128 ) {
-    window.alert("enter between 8 and 128");
-    passwordLength= prompt("choose password length between 8-128 characters");
+  while (passwordLength < 8 || passwordLength > 128||isNaN(passwordLength)===true) {
+    window.alert("enter password length from 8 to 128");
+    passwordLength= prompt("choose password length from 8 to 128 characters");
   }
-
-  var uppercaseCheck= prompt("include upper-case? (Y/N)").toUpperCase();
-  var lowercaseCheck= prompt("include lower-case? (Y/N)").toUpperCase();
-  var numberCheck= prompt("include numbers? (Y/N)").toUpperCase();
+  while (validation===false){
+    var uppercaseCheck= prompt("include upper-case? (Y/N)").toUpperCase();
+    var lowercaseCheck= prompt("include lower-case? (Y/N)").toUpperCase();
+    var numberCheck= prompt("include numbers? (Y/N)").toUpperCase();
+    var specialcharCheck= prompt("include special characters? (Y/N)").toUpperCase();
+    
+    validation = window.confirm("confirm the following : " + "upper case(" + uppercaseCheck + "), lower case("+ lowercaseCheck + "), numbers(" + numberCheck + "), special characters(" + specialcharCheck + ")");
+  
+  }
 
   if (uppercaseCheck === "Y"){
     chars=chars.concat(upperCase);
@@ -41,19 +47,27 @@ function generatePassword() {
     chars = chars.concat(numbers);
   }
 
+  if(specialcharCheck === "Y"){
+    chars=chars.concat(specialChar);
+  }
+
   for(randomPass.length;randomPass.length<passwordLength;){
-    if (uppercaseCheck === "Y"){
+    if (uppercaseCheck === "Y" && randomPass.length<passwordLength){
       randomNum= Math.floor(Math.random()*upperCase.length);
       randomPass+=upperCase.charAt(randomNum);
     }
-    if (lowercaseCheck === "Y"){
+    if (lowercaseCheck === "Y"&& randomPass.length<passwordLength){
       randomNum= Math.floor(Math.random()*lowerCase.length);
       randomPass+=lowerCase.charAt(randomNum);
-    }
 
-    if (numberCheck === "Y"){
+    }
+    if (numberCheck === "Y" && randomPass.length<passwordLength){
       randomNum= Math.floor(Math.random()*numbers.length);
       randomPass+=numbers.charAt(randomNum);
+    }
+    if (specialcharCheck === "Y" && randomPass.length<passwordLength){
+      randomNum= Math.floor(Math.random()*specialChar.length);
+      randomPass+=specialChar.charAt(randomNum);
     }
   }
 return randomPass;
